ffc4491048c1e8aa016654dd5e3c3a1e105d0a61
rojinyilmaz <70367334+rojinyilmaz@users.noreply.github.com>
2021-07-21 11:33:57 +0200

    Added database.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+
+const PORT = 1337
+
+app.get('/', (req, res) => {
+console.log('GET /');
+res.send('Yes I am working')
+})
+
+app.listen(PORT, () => {
+	console.log('Server is listening on port ' + PORT);
+})
+
+
